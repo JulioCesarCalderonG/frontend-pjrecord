782dac5fb674f24cf0760d68f9a36d10d2819c00
Julio Cesar Calderon Galindo <julcescalgalty@gmail.com>
2023-06-08 13:03:31 -0500

    cambios en el diseño

--- src/app/admin/reportes/reportes.component.ts
+++ src/app/admin/reportes/reportes.component.ts
@@ -1,21 +1,271 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { AreaService } from 'src/app/servicios/area.service';
+import { CargoService } from 'src/app/servicios/cargo.service';
+import { DependenciaService } from 'src/app/servicios/dependencia.service';
+import { GeneralService } from 'src/app/servicios/general.service';
+import { OrganoService } from 'src/app/servicios/organo.service';
+import { TipoPersonalService } from 'src/app/servicios/tipo-personal.service';
+import { TipodocumentoService } from 'src/app/servicios/tipodocumento.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-reportes',
   templateUrl: './reportes.component.html',
   styleUrls: ['./reportes.component.css']
 })
 export class ReportesComponent implements OnInit {
-
+  idpersonal?:string|number;
   personal='';
-  constructor(private rutaActiva: ActivatedRoute) { }
-
-  ngOnInit(): void {
-    console.log(this.rutaActiva.snapshot.params.id);
-    this.personal = this.rutaActiva.snapshot.params.personal;
+  listTipodocumento?:Array<any>;
+  listCargo?:Array<any>;
+  listAutoriza?:Array<any>;
+  listDependencia?:Array<any>;
+  listTipoPersonal?:Array<any>;
+  generalForm:FormGroup;
+  loadImage: string='';
+  @ViewChild('fileInput', { static: false }) fileInput?: ElementRef;
+  uploadFiles?: File;
+  constructor(
+    private rutaActiva: ActivatedRoute,
+    private generalService:GeneralService,
+    private tipodocumentoService:TipodocumentoService,
+    private tipoPersonalServicie:TipoPersonalService,
+    private cargoService:CargoService,
+    private organoService:OrganoService,
+    private unidadService:DependenciaService,
+    private areaService:AreaService,
+    private fb:FormBuilder
+  ) {
+    this.generalForm = this.fb.group({
+      tipodocumento:['',Validators.required],
+      numero:[''],
+      ano:[''],
+      tiposigla:['0'],
+      autoriza:['0'],
+      tipodependencia:['',Validators.required],
+      dependencia:['',Validators.required],
+      cargo:['',Validators.required],
+      desde:['', Validators.required],
+      hasta:['']
+    });
   }
 
+  ngOnInit(): void {
+    this.idpersonal= this.rutaActiva.snapshot.params.id;
+    this.personal = this.rutaActiva.snapshot.params.personal;
+    this.mostrartipodocumento();
+    this.mostrarTipoPersonal();
+  }
+  registrarGeneral(){
+    const formData = new FormData();
+    formData.append('tipo_documento',this.generalForm.get('tipodocumento')?.value);
+    formData.append('numero',this.generalForm.get('numero')?.value);
+    formData.append('ano',this.generalForm.get('ano')?.value);
+    formData.append('tipo_sigla',this.generalForm.get('tiposigla')?.value);
+    formData.append('autoriza',this.generalForm.get('autoriza')?.value);
+    formData.append('tipo_dependencia',this.generalForm.get('tipodependencia')?.value);
+    formData.append('dependencia',this.generalForm.get('dependencia')?.value);
+    formData.append('id_cargo',this.generalForm.get('cargo')?.value);
+    formData.append('desde',this.generalForm.get('desde')?.value);
+    formData.append('hasta',this.generalForm.get('hasta')?.value);
+    formData.append('id_personal',`${this.idpersonal}`);
+    if (this.loadImage==='') {
+      Swal.fire({
+        position: 'top-end',
+        icon: 'warning',
+        title: 'Seleccione el documento que autoriza',
+        showConfirmButton: false,
+        timer: 1500
+      });
+    }else{
+      formData.append('archivo',this.fileInput?.nativeElement.files[0]);
+      this.generalService.postGeneral(formData).subscribe(
+        (data)=>{
+          Swal.fire(
+            'Registrado!',
+            data.msg,
+            'success'
+          )
+          this.cancelar();
+          this.reset();
+        },(error)=>{
+          console.log(error);
 
+        }
+      )
+    }
 
+  }
+  mostrartipodocumento(){
+    this.tipodocumentoService.getTipodocumento().subscribe(
+      (data)=>{
+        this.listTipodocumento = data.resp;
+      }, (error)=>{
+        console.log(error);
+      }
+    )
+  }
+  mostrarTipoPersonal(){
+    this.tipoPersonalServicie.getTipoPersonal('1').subscribe(
+      (data)=>{
+        this.listTipoPersonal = data.resp;
+      },(error)=>{
+        console.log(error);
 
+      }
+    )
+  }
+  buscarSigla(event:any){
+    switch (event.target.value) {
+      case '1':
+          this.organoService.getOrgano().subscribe(
+            (data)=>{
+              console.log(data);
+              this.listAutoriza = data.resp;
+            },(error)=>{
+              console.log(error);
+            }
+          );
+          return;
+      case '2':
+        this.unidadService.getUnidad().subscribe(
+          (data)=>{
+            console.log(data);
+            this.listAutoriza = data.resp;
+          },(error)=>{
+            console.log(error);
+          }
+        );
+        return;
+      case '3':
+        this.areaService.getAreas().subscribe(
+          (data)=>{
+            console.log(data);
+            this.listAutoriza = data.resp;
+          },(error)=>{
+            console.log(error);
+          }
+        );
+        return;
+      default:
+        this.listAutoriza=[]
+        return;
+    }
+  }
+  buscarDependencia(event:any){
+    switch (event.target.value) {
+      case '1':
+          this.organoService.getOrgano().subscribe(
+            (data)=>{
+              console.log(data);
+              this.listDependencia = data.resp;
+            },(error)=>{
+              console.log(error);
+            }
+          );
+          return;
+      case '2':
+        this.unidadService.getUnidad().subscribe(
+          (data)=>{
+            console.log(data);
+            this.listDependencia = data.resp;
+          },(error)=>{
+            console.log(error);
+          }
+        );
+        return;
+      case '3':
+        this.areaService.getAreas().subscribe(
+          (data)=>{
+            console.log(data);
+            this.listDependencia = data.resp;
+          },(error)=>{
+            console.log(error);
+          }
+        );
+        return;
+      default:
+        this.listDependencia=[]
+        return;
+    }
+  }
+  filtrarTipoPersonal(event:any){
+    const tipo =event.target.value;
+    if (!tipo) {
+      this.cargoService.getCargoPersonal(0).subscribe(
+        (data)=>{
+          this.listCargo = data.resp;
+        },(error)=>{
+          console.log(error);
+
+        }
+      )
+    }else{
+      this.cargoService.getCargoPersonal(tipo).subscribe(
+        (data)=>{
+          this.listCargo = data.resp;
+        },(error)=>{
+          console.log(error);
+
+        }
+      )
+    }
+  }
+  capturarFile(event: any) {
+    this.uploadFiles = event.target.files[0];
+
+    if (this.uploadFiles!.size > 2500000) {
+      Swal.fire({
+        position: 'top-end',
+        icon: 'warning',
+        title: 'El tamaño maximo es de 20MB',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.reset();
+      this.loadImage = '';
+    }else{
+      this.loadImage='cargado';
+    }
+
+  }
+  reset() {
+    this.fileInput!.nativeElement.value = '';
+  }
+  extraserBase64 = async ($event: any) =>
+    new Promise((resolve, reject) => {
+      try {
+        /* const unsafeImg = window.URL.createObjectURL($event);
+      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg); */
+        const reader = new FileReader();
+        reader.readAsDataURL($event);
+        reader.onload = () => {
+          resolve({
+            base: reader.result,
+          });
+        };
+        reader.onerror = (error) => {
+          resolve({
+            base: null,
+          });
+        };
+      } catch (e) {
+        reject(e);
+      }
+    });
+    cancelar(){
+      this.generalForm.setValue({
+        tipodocumento:'',
+        numero:'',
+        ano:'',
+        tiposigla:'0',
+        autoriza:'0',
+        tipodependencia:'',
+        dependencia:'',
+        cargo:'',
+        desde:'',
+        hasta:''
+      })
+    }
 }
